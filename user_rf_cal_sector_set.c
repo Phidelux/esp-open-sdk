26860f78e36b7ced828751ccc43415b7f4fa0b7e
Andreas Schickedanz <as@bornageek.com>
2020-05-03 09:53:53 +0200

    Revert "makefile changes for newer ct-ng"
    This reverts commit cad5d8d15a6b2c988dcc771b4dde8cf44fc462c4.

--- user_rf_cal_sector_set.c
+++ user_rf_cal_sector_set.c
@@ -1,7 +1,6 @@
 #include <c_types.h>
 #include <spi_flash.h>
 
-__attribute__ ((weak))
 uint32 user_rf_cal_sector_set(void) {
     extern char flashchip;
     SpiFlashChip *flash = (SpiFlashChip*)(&flashchip + 4);
@@ -10,4 +9,3 @@ uint32 user_rf_cal_sector_set(void) {
     uint32_t sec_num = flash->chip_size >> 12;
     return sec_num - 5;
 }
-
